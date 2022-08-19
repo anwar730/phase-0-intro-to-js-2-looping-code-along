f2803462d6b09a701e3556c645273a15c4ff9281
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-08-19 06:50:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names=["Anwar","Yasin","Yahya"]
+const event='birthday'
+function writeCards(names,event){
+    let thankyou=[]
+    for(let i=0;i<names.length;i++){
+        let messages=`Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        thankyou.push(messages);
+    }
+    return thankyou
+
+}
+function countDown(n){
+    let i=n
+    while (i>=0){
+        console.log(i)
+        i--
+    }
+}
+countDown(10)
